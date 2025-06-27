cae4913ee68da84f91a8e717fb2a41a9e86a840c
Pankaj Bisht <pankajbishtuk04@gmail.com>
2025-06-27 21:26:44 +0530

    gpt-changes

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -3,6 +3,7 @@ import userReducer from "./userSlice"
 import moviesReducer from "./moviesSlice";
 import gptSlice from "./gptSlice"
 
+
 const appStore = configureStore({
     reducer: {
         user: userReducer,
